dc06e859950b934ccb7840d9d6893f41dc147af9
LagMeester4000 <lagmeester4000@gmail.com>
2019-12-20 13:13:42 +0100

    Changed main loop to wait for keyboard input instead

--- KraPunch/source/KraPunch/main.cpp
+++ KraPunch/source/KraPunch/main.cpp
@@ -3,6 +3,42 @@
 #include <chrono>
 #include <thread>
 
+#if _WIN32
+#include<stdio.h>
+#include<conio.h>
+#elif __linux__
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+int kbhit(void)
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if (ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return 1;
+	}
+
+	return 0;
+}
+#endif
+
+
 class ForceExitScopeTest {
 public:
 	ForceExitScopeTest() 
@@ -21,9 +57,9 @@ int main()
 	using namespace std::chrono;
 
 	ForceExitScopeTest o;
-
-	// runs for 20 seconds
-	for (size_t i = 0; i < 20; ++i)
+	
+	// Run until any key is pressed
+	while (!kbhit())
 	{
 
 		seconds sec(1);
